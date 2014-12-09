88a5f0dc794e97449e8c75b82b2b952fc1120e9c
Bas Stottelaar <basstottelaar@gmail.com>
2014-12-09 10:54:35 +0100

    Lower temperature.

--- firmware/inc/config.h
+++ firmware/inc/config.h
@@ -5,7 +5,7 @@
 #define PWM_FREQUENCY (50000)
 
 /* Target temperature (C) */
-#define TARGET_TEMPERATURE (60.0)
+#define TARGET_TEMPERATURE (40.0)
 
 /* Minimal fan speed (%) */
 #define MINIMAL_FAN_SPEED (10)
