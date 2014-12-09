28feb237d596f2647ab641190073e6e994560d3b
Bas Stottelaar <basstottelaar@gmail.com>
2014-12-09 21:13:10 +0100

    Monitor multiple sensors.

--- contrib/pid_plot.py
+++ contrib/pid_plot.py
@@ -9,11 +9,12 @@ def main():
     Monitor stdin for incoming data and parse it. When the stream closes, the
     graph will be plotted.
 
-    Format of incoming data: time,current,target,control.
+    Format of data: i,temperature_i,target_i,speed_i,pid_i,...,max_speed
     """
 
     # Gather data
     start = time.time()
+    sensors = None
     data = []
 
     try:
@@ -27,11 +28,19 @@ def main():
             # Split data
             line = line.strip().split(",")
 
-            if len(line) < 5:
+            # Determine number of sensors from first line of data
+            if (len(line) - 2) % 5 == 0:
+                if sensors is None:
+                    sensors = (len(line) - 2) / 5
+            else:
+                continue
+
+            # Check if all data is available
+            if len(line) != sensors * 5 + 2:
                 continue
 
             # Parse the data
-            data.append(map(int, line[:5]))
+            data.append(map(int, line))
     except KeyboardInterrupt:
         pass
 
@@ -41,20 +50,30 @@ def main():
 
     # Generate graph
     figure = plot.figure()
-    axis = figure.add_subplot(1, 1, 1)
-    axis2 = axis.twinx()
+    axis = None
 
-    times, values, targets, controls, pids = zip(*data)
-    axis.plot(times, targets, color="grey", marker=".")
-    axis.plot(times, values, color="black")
-    axis2.plot(times, controls, color="red")
-    #axis2.plot(times, pids, color="green")
+    times = zip(*data)[0]
+    max_speeds = zip(*data)[-1]
 
-    axis.set_xlabel("Time (s)")
-    axis.set_ylabel("Temperature (C)")
-    axis2.set_ylabel("Control (%)")
+    for i in xrange(sensors):
+        _, temperatures, targets, speeds, pids = zip(*data)[i*5 + 1:i*5 + 6]
 
-    axis.grid()
+        axis = figure.add_subplot(1, sensors, i + 1, sharey=axis)
+        axis2 = axis.twinx()
+
+        axis.plot(times, targets, color="b", label="Target")
+        axis.plot(times, temperatures, color="c", label="Temperature")
+        axis2.plot(times, speeds, color="y", label="Speed")
+        axis2.plot(times, max_speeds, color="r", label="Max Speed")
+
+        axis.set_xlabel("Time (s)")
+        axis.set_ylabel("Temperature (C)")
+        axis2.set_ylabel("Control (%)")
+
+        axis.set_ylim(bottom=0, top=1000)
+        axis2.set_ylim(bottom=0, top=100)
+
+        axis.grid()
 
     # Show
     plot.show()
